6039b7330ee0bb7e2e1f0333eaa0b95e9964c5cb
simonpelz <simon@pelz-hd.de>
2024-08-21 18:29:36 +0200

    streamlined and compatible with new dataloader

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 import torch
 import nlpaug.augmenter.word as naw
-from data import MultiViewTextDataset
+from data import AbstractMultiViewTextDataset
 from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
 from torch.utils.data import DataLoader
 import torch.optim as optim
@@ -31,38 +31,20 @@ def main():
 
     # Use only a subset of the training dataset for testing
     subset_train_dataset = pre_dataset["train"].select(range(20))
-    mv_train_dataset = MultiViewTextDataset(subset_train_dataset, tokenizer, transform_list=transform_list)
-
-    test_dataset = pre_dataset["validation"]
-    tokens_test_dataset = test_dataset.map(
-        lambda example: tokenizer(example["sentence"], max_length=128, padding='max_length', truncation=True),
-        batched=True
-    )
-
-    mv_train_loader = DataLoader(mv_train_dataset, shuffle=False, batch_size=BATCH_SIZE)
-    test_dataloader = DataLoader(tokens_test_dataset, shuffle=False, batch_size=BATCH_SIZE)
+    mv_train_loader = AbstractMultiViewTextDataset(subset_train_dataset, tokenizer, transform_list=transform_list).__dataloader__(BATCH_SIZE)
 
     # Model configuration
     config = AutoConfig.from_pretrained(MODEL_NAME)
     config.num_labels = num_labels
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=config)
-
-    # Validate and fix the model for differential privacy if needed
-    if not ModuleValidator.is_valid(model):
-        model = ModuleValidator.fix(model)
-
-    model.train() #TODO does this belong somewhere else?
-
-    device_name = "cuda" if torch.cuda.is_available() else "cpu"
-    device = torch.device(device_name)
-    print(device_name)
-    model.to(device)
+    print(f"num labels: {num_labels}")
 
     # Differential privacy parameters
     EPSILON = 8.0
     DELTA = 1 / len(mv_train_loader)
     MAX_GRAD_NORM = 1.0
 
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=config)
+    model.train()
     optimizer = optim.SGD(model.parameters(), lr=LR)
 
     privacy_engine = PrivacyEngine()
@@ -78,7 +60,8 @@ def main():
     with BatchMemoryManager(data_loader=dp_train_loader, max_physical_batch_size=32, optimizer=dp_optimizer) as memory_safe_data_loader: 
         for batch in memory_safe_data_loader:  # Iterating through augmented batches
             print(f"\n NEW BATCH"+"="*100+"\n")
-            for sample in batch[0]:
+            #print(f"stacked views batch: {batch}")
+            for sample in batch['input_ids']:
                 print(f"\n new sample: -------------------------------------\n")
                 for view in sample:
                     # Convert tokens back to text and print them
