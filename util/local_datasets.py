7748766b6a3cf2862e98883861ab17bf17d113f1
simonpelz <simon@pelz-hd.de>
2024-10-23 17:08:39 +0200

    Local dataset loading on cluster

--- util/local_datasets.py
+++ util/local_datasets.py
@@ -1,11 +1,13 @@
 
 import csv
 from torch.utils.data import Dataset
+import torch
 
 class LocalTSVDataset(Dataset):
     def __init__(self, tsv_file):
         self.tsv_file = tsv_file
         self.samples = self._load_data()
+        self.column_names = ['sentence1','sentence2','label']
 
     def _load_data(self):
         samples = []
@@ -30,4 +32,52 @@ class LocalTSVDataset(Dataset):
     def select(self,indices):
         self.samples = [self.samples[i] for i in indices]
         return self
+
+    def map(self, function, batched=False, batch_size=32):
+        """Apply a function to each sample or batch of samples in the dataset. Batched map not supported"""
+        
+        # ignore batched directive
+        batched = False
+        if batched:
+            # Process the dataset in batches
+            new_samples = []
+            for i in range(0, len(self.samples), batch_size):
+                batch = self.samples[i:i+batch_size]
+                new_batch = function(batch)
+                new_samples.extend(new_batch)
+            self.samples = new_samples
+        else:
+            # Apply the function to each individual sample
+            for sample in self.samples:
+                sample.update(function(sample))
+        return self  # Returning self allows for method chaining
+
+    def rename_column(self, old_column_name, new_column_name):
+        """Rename a column (key in sample dict) from old_column_name to new_column_name."""
+        for sample in self.samples:
+            if old_column_name in sample:
+                sample[new_column_name] = sample.pop(old_column_name)
+        return self  # Returning self allows for method chaining
+
+
+    def set_format(self, type=None, columns=None):
+        """Only type == torch !! Set the format of the dataset and select specific columns for PyTorch conversion."""
+        if type == 'torch':
+            # Convert specified columns to PyTorch tensors
+            for i, sample in enumerate(self.samples):
+                for column in columns:
+                    if column in sample:
+                        self.samples[i][column] = torch.tensor(sample[column])
+        else: raise NotImplementedError
+        return self  # Returning self allows for method chaining
+
+    def remove_columns(self, column_names):
+        """Rename a column (key in sample dict) from old_column_name to new_column_name."""
+        for name in column_names:
+            for sample in self.samples:
+                if name in sample:
+                    sample.pop(name)
+                else:
+                    break
+        return self  # Returning self allows for method chaining
 
